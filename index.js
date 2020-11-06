932807da0e35a14966f74f92a30bf42a57c756d4
Minna Comitre <mkishere.oficial@gmail.com>
2020-11-06 18:29:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const path = require('path');
+
+const fs = require('fs');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+//const livereload = require("livereload");
+
+const publicDirectory = path.join(__dirname, 'public');
+
+//const liveReloadServer = livereload.createServer();
+//liveReloadServer.watch(publicDirectory);
+//const connectLivereload = require("connect-livereload");
+
+/*
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+*/
+
+//app.use(connectLivereload());
+app.use(express.static(publicDirectory));
+app.set('views', publicDirectory);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+app.get('/', (request, response) => {
+
+	const date = new Date;
+	let hours = (date.getHours() <= 9) ? "0" + date.getHours() : date.getHours(), minutes = (date.getMinutes() <= 9) ? "0" + date.getMinutes() : date.getMinutes(), seconds = (date.getSeconds() <= 9) ? "0" + date.getSeconds() : date.getSeconds();
+	let time = `${hours}:${minutes}:${seconds}`;
+
+	//console.log(`Request recebida às ${time}`);
+
+	response.render('chat.html');
+});
+
+function fetchMessages () {
+	let fetched = [];
+	if (!fs.existsSync(`./messages.json`)) {
+		fs.writeFileSync(`./messages.json`, JSON.stringify(fetched));
+	} else {
+	fetched = require(`./messages.json`);
+	}
+
+	return fetched;
+}
+
+
+let messages = fetchMessages();
+
+const ytdl = require('ytdl-core');
+
+io.on('connection', socket => {
+	console.log(`Socket conectado: ${socket.id}`);
+
+	socket.emit('previusMessages', messages);
+
+	const date = new Date;
+	let hours = (date.getHours() <= 9) ? "0" + date.getHours() : date.getHours(), minutes = (date.getMinutes() <= 9) ? "0" + date.getMinutes() : date.getMinutes(), seconds = (date.getSeconds() <= 9) ? "0" + date.getSeconds() : date.getSeconds();
+	let time = `${hours}:${minutes}:${seconds}`;
+
+	socket.on('sendMessage', data => {
+		messages.push(data);
+		
+		fs.writeFileSync(`./messages.json`, JSON.stringify(messages));
+
+		socket.broadcast.emit('receivedMessage', data);
+	});
+	socket.on('notifyMessage', data => {
+		
+		if (!data.receivedAt || !data.author) {
+			return;
+		}
+
+		if (data.author != data.receivedAt && data.receivedAt && data.author) {
+			console.log(`1 Mensagem recebida por ${data.author} em ${data.receivedAt}`);
+			socket.emit('notification', data);
+		}
+	});
+
+	const ytSearch = require('./modules/mkishereoficial/yt_search');
+
+	socket.on('getMusic', async (searched) => {
+		let log = console.log(searched) || fs.writeFileSync('./background-1.log', searched);
+	});
+
+});
+
+console.log(`App Iniciado.`);
+
+server.listen(3000);
---
+++ modules/mkishereoficial/yt_search/main.js
@@ -0,0 +1,145 @@
+const version = require("./package.json").version;
+
+const yts = require( 'yt-search' );
+
+let language = "pt";
+let translate = require('@vitalets/google-translate-api');
+async function traduzir (value) {
+	let data = "Unavailable";
+	await translate(value, {to: language}).then(async (res) => {
+		data = res.text;
+	})
+	return data;
+}
+
+async function search (value) {
+	if (!value) throw new Error("O Valor da Pesquisa não foi identificado. VVerifique se é uma String.");
+
+	const r = await yts(value);
+
+	const videos = r.videos.slice( 0, 5 );
+
+	let video = videos[0];
+
+	let obj = {
+		duration: video.timestamp,
+		title: video.title,
+		url: video.url,
+		id: video.videoId,
+		description: video.description,
+		imageURL: function get () {
+			return video.image;
+		},
+		thumbnailURL: function get () {
+			return video.thumbnail;
+		},
+		ago: (async (event) => {
+			let a = video.ago;
+			let b = await traduzir("Indisponível");
+
+			if (!a) {
+				return b;
+			}
+
+			await translate(a, {to: language}).then(res => {
+				return b = res.text;
+			}).catch(err => {
+    		throw console.error(err);
+			});
+
+      if (language != "en") {
+			     return b;
+      }
+
+      return a;
+		})(),
+		author: {
+			name: video.author.name,
+			url: video.author.url,
+			},
+
+		views: video.views.toLocaleString(),
+		downloadURL: function get () {
+			let url = `https://www.y2mate.com/pt/youtube/${this.id}`;
+
+			return url;
+		}
+
+	};
+
+  return obj;
+}
+async function multi_search(searchValue, maxVideos) {
+	if (searchValue.length <= 0) throw new Error("O Valor da Pesquisa não foi identificado. Verifique se é uma String.");
+
+	if (!maxVideos) {
+		console.log(`Você não espeficou o parâmetro maxVideos. Este deve ser um número, no qual especifica-se o número máximo de vídeos no array.`)
+		maxVideos = 5;
+	} else if (isNaN(maxVideos)) {
+		throw new Error("O parâmetro maxVideos deve ser um número, no qual especifica-se o número máximo de vídeos no array.");
+	}
+
+	const r = await yts(searchValue);
+	if (!r.videos) throw new Error("Nenhum vídeo foi encontrado.");
+
+	const vids = r.videos.slice( 0, maxVideos );
+	const videos = await vids.map(async (video) => {
+		const obj = {
+			duration: video.timestamp,
+			title: video.title,
+			url: video.url,
+			id: video.videoId,
+			description: video.description,
+			imageURL: function get () {
+				return video.image;
+			},
+			thumbnailURL: function get () {
+				return video.thumbnail;
+			},
+			ago: (async (event) => {
+				let a = video.ago;
+				let b = await traduzir("Indisponível");
+
+				if (!a) {
+					return b;
+				}
+
+				await translate(a, {to: language}).then(res => {
+					return b = res.text;
+				}).catch(err => {
+	    		throw console.error(err);
+				});
+
+	      if (language != "en") {
+				     return b;
+	      }
+
+	      return a;
+			})(),
+			author: {
+				name: video.author.name,
+				url: video.author.url,
+				},
+
+			views: video.views.toLocaleString(),
+			downloadURL: function get () {
+				let url = `https://www.y2mate.com/pt/youtube/${this.id}`;
+
+				return url;
+			}
+
+		};
+
+	  return obj;
+	});
+
+	if (!videos) throw new Error("Nenhum vídeo foi encontrado.");
+
+	return videos;
+}
+
+module.exports = search;
+module.exports.multi = multi_search;
+module.exports.search = multi_search;
+module.exports.language = language;
+module.exports.version = version;
